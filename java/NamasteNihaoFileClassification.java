c26998ac48844e97d560641258569c6999e8b0da
fysteven <yaofan@usc.edu>
2016-03-04 10:37:06 -0800

    update

--- java/NamasteNihaoFileClassification.java
+++ java/NamasteNihaoFileClassification.java
@@ -2,6 +2,7 @@
  * Created by Frank on 3/1/16.
  */
 
+import com.uwyn.jhighlight.fastutil.Hash;
 import org.apache.tika.Tika;
 
 import java.io.File;
@@ -21,8 +22,9 @@ import org.json.simple.JSONObject;
 public class NamasteNihaoFileClassification {
     public static String cwd = System.getProperty("user.dir") + "/";
     public HashMap<String, String> fileTypeMap = new HashMap<>();
-    public HashMap<String, HashMap> typeFileMap = new HashMap<>();
-    public HashMap<String, HashMap> oldTypeFileMap = new HashMap<>();
+    public HashMap<String, HashMap<String, Integer>> typeFileMap = new HashMap<>();
+    public HashMap<String, HashMap<String, Integer>> oldTypeFileMap = new HashMap<>();
+    public HashMap<String, HashMap<String, Integer>> typeCountMap = new HashMap<>();
     public HashMap<String, Integer> duplicateMap = new HashMap<>();
 /*
     public static void main(String[] args) throws Exception {
@@ -43,9 +45,21 @@ public class NamasteNihaoFileClassification {
     public static  void main(String[] args) throws Exception {
         NamasteNihaoFileClassification fileClassification = new NamasteNihaoFileClassification();
         //System.out.println(args);
+
+        for (String string : args) {
+            System.out.println(string);
+        }
+
+        fileClassification.prepareProcessing();
+
         ArrayList<String> filepaths = fileClassification.getAllFilepaths(Arrays.copyOfRange(args, 0, args.length));
         fileClassification.identifyFileType(filepaths);
-        fileClassification.dumpAllMapsToJSON(System.getProperty("user.dir") + "/nmstOutput.json");
+        fileClassification.dumpAllMapsToJSON(System.getProperty("user.dir") + "/output");
+    }
+
+    public void prepareProcessing() {
+        this.typeCountMap.put("old", new HashMap<>());
+        this.typeCountMap.put("new", new HashMap<>());
     }
 
     public ArrayList<String> getAllFilepaths(String[] args) {
@@ -68,6 +82,14 @@ public class NamasteNihaoFileClassification {
                         }
 
                         HashMap<String, Integer> currentFileMap = oldTypeFileMap.get(file.getParentFile().getName());
+                        if (!currentFileMap.containsKey(file.getName())) {
+                            if (typeCountMap.get("old").containsKey(file.getParentFile().getName())) {
+                                HashMap<String, Integer> countMap = typeCountMap.get("old");
+                                countMap.put(file.getParentFile().getName(), countMap.get(file.getParentFile().getName()) + 1);
+                            } else {
+                                typeCountMap.get("old").put(file.getParentFile().getName(), 1);
+                            }
+                        }
                         currentFileMap.put(file.getName(), 1);
                     }
 
@@ -92,14 +114,18 @@ public class NamasteNihaoFileClassification {
         return fileList;
     }
 
-    public void identifyFileType(ArrayList<String> filepaths) {
+    public void identifyFileType(ArrayList<String> filepaths) throws IOException {
         System.out.println("Start identifying");
 
         Tika tika = new Tika();
-
         filepaths.forEach((item) -> {
                     File file = new File(item);
-                    String fileType = tika.detect(item);
+                    String fileType = null;
+                    try {
+                        fileType = tika.detect(file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     if (fileTypeMap.containsKey(file.getName())) {
                         if (duplicateMap.containsKey(file.getName())) {
                             duplicateMap.put(file.getName(), duplicateMap.get(file.getName()) + 1);
@@ -109,9 +135,16 @@ public class NamasteNihaoFileClassification {
                     } else {
                         fileTypeMap.put(file.getName(), fileType);
                         if (!typeFileMap.containsKey(fileType)) {
-                            HashMap<String, Integer> fileMap = new HashMap<String, Integer>();
-                            typeFileMap.put(fileType, fileMap);
+                            typeFileMap.put(fileType, new HashMap<String, Integer>());
                         }
+
+                        if (typeCountMap.get("new").containsKey(fileType)) {
+                            HashMap<String, Integer> countMap = typeCountMap.get("new");
+                            countMap.put(fileType, countMap.get(fileType) + 1);
+                        } else {
+                            typeCountMap.get("new").put(fileType, 1);
+                        }
+
                         HashMap<String, Integer> fileMap = (HashMap<String, Integer>) typeFileMap.get(fileType);
                         fileMap.put(file.getName(), 1);
                     }
@@ -119,19 +152,45 @@ public class NamasteNihaoFileClassification {
         );
 
 
+
     }
 
+    @SuppressWarnings("unchecked")
     public void dumpAllMapsToJSON(String jsonFilePath) throws IOException {
         System.out.println("Start dump all maps to json");
-        JSONObject json = new JSONObject();
-        json.put("oldTypeFile", this.oldTypeFileMap);
-        json.put("typeFile", this.typeFileMap);
-        json.put("fileType", this.fileTypeMap);
-        json.put("duplicate", this.duplicateMap);
+        JSONObject json = new JSONObject(typeCountMap);
 
-        FileWriter fileWriter = new FileWriter(jsonFilePath);
+        /*
+        JSONObject json2 = new JSONObject();
+        JSONObject json3 = new JSONObject();
+        JSONObject json4 = new JSONObject();
+
+        json.put("oldTypeFile", this.oldTypeFileMap);
+        json2.put("typeFile", this.typeFileMap);
+        json3.put("fileType", this.fileTypeMap);
+        json4.put("duplicate", this.duplicateMap);
+        */
+        System.out.println("Writing to JSON files");
+
+        FileWriter fileWriter = new FileWriter(jsonFilePath + "typeCount.json");
         fileWriter.write(json.toJSONString());
         fileWriter.flush();
         fileWriter.close();
+        /*
+        FileWriter fileWriter2 = new FileWriter(jsonFilePath + "2.json");
+        fileWriter2.write(json2.toJSONString());
+        fileWriter2.flush();
+        fileWriter2.close();
+
+        FileWriter fileWriter3 = new FileWriter(jsonFilePath + "3.json");
+        fileWriter3.write(json3.toJSONString());
+        fileWriter3.flush();
+        fileWriter3.close();
+
+        FileWriter fileWriter4 = new FileWriter(jsonFilePath + "4.json");
+        fileWriter4.write(json4.toJSONString());
+        fileWriter4.flush();
+        fileWriter4.close();
+        */
     }
 }
